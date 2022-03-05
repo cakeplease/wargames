c55a65031ef4f92da8017333ea336571fe19a752
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-05 19:13:36 +0100

    Added simulate method

--- src/main/java/no/ntnu/katarzsz/Battle.java
+++ src/main/java/no/ntnu/katarzsz/Battle.java
@@ -4,14 +4,30 @@ public class Battle {
     private Army armyOne;
     private Army armyTwo;
 
+    private boolean hasWinner = false;
+
     public Battle(Army armyOne, Army armyTwo) {
         this.armyOne = armyOne;
         this.armyTwo = armyTwo;
     }
 
-    /*private Army simulate() {
-        //simulate behavior here
-    }*/
+    private Army simulate() {
+        while (this.hasWinner == false) {
+            if (this.armyOne.hasUnits() && this.armyTwo.hasUnits()) {
+                this.armyOne.getRandom().attack(this.armyTwo.getRandom());
+                if (this.armyTwo.hasUnits()) {
+                    this.armyTwo.getRandom().attack(this.armyOne.getRandom());
+                }
+            } else if (!this.armyOne.hasUnits()) {
+                this.hasWinner = true;
+                return this.armyOne;
+            } else if (!this.armyTwo.hasUnits()) {
+                this.hasWinner = true;
+                return this.armyTwo;
+            }
+        }
+        return null;
+    }
 
     @Override
     public String toString() {
