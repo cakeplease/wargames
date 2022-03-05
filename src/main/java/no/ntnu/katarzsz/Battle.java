2f625b1d5a9212d39b98e3f349b14d60859e53d5
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-05 10:59:39 +0100

    Added Battle class

---
+++ src/main/java/no/ntnu/katarzsz/Battle.java
@@ -0,0 +1,23 @@
+package no.ntnu.katarzsz;
+
+public class Battle {
+    private Army armyOne;
+    private Army armyTwo;
+
+    public Battle(Army armyOne, Army armyTwo) {
+        this.armyOne = armyOne;
+        this.armyTwo = armyTwo;
+    }
+
+    /*private Army simulate() {
+        //simulate behavior here
+    }*/
+
+    @Override
+    public String toString() {
+        return "Battle{" +
+                "armyOne=" + armyOne +
+                ", armyTwo=" + armyTwo +
+                '}';
+    }
+}
