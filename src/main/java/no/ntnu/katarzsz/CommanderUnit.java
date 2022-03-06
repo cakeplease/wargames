98e690c112c06800812363cffbed528ccd2e6534
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 16:00:55 +0100

    Added javadoc

--- src/main/java/no/ntnu/katarzsz/CommanderUnit.java
+++ src/main/java/no/ntnu/katarzsz/CommanderUnit.java
@@ -1,12 +1,26 @@
 package no.ntnu.katarzsz;
 
+/**
+ * CommanderUnit class which extends CavalryUnit
+ */
 public class CommanderUnit extends CavalryUnit{
+    /**
+     * CommanderUnit constructor with all params
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
     public CommanderUnit(String name, int health, int attack, int armor) {
         super(name, health, attack, armor);
     }
+
+    /**
+     * CommanderUnit constructor with only name and health params
+     * @param name
+     * @param health
+     */
     public CommanderUnit(String name, int health) {
         super(name, health, 25, 15);
     }
-
-    //Enheten har samme bonusberegninger som CavalryUnit
 }
