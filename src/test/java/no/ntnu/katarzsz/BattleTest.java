8ea08c6a027b38cc8eab16ae8a0dd03ab8d08c88
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 16:14:14 +0100

    Added BattleTest class

--- src/test/java/no/ntnu/katarzsz/BattleTest.java
+++ src/test/java/no/ntnu/katarzsz/BattleTest.java
@@ -2,8 +2,29 @@ package no.ntnu.katarzsz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class BattleTest {
 
+    @Test
+    public void testSimulation() {
+        CommanderUnit unit1 = new CommanderUnit("Unit1", 20);
+        RangedUnit unit2 = new RangedUnit("Unit2", 10);
 
+        CommanderUnit unit3 = new CommanderUnit("Unit3", 20);
+        RangedUnit unit4 = new RangedUnit("Unit4", 10);
 
+        Army armyOne = new Army("ArmyOne");
+        Army armyTwo = new Army("ArmyTwo");
+
+        armyOne.add(unit1);
+        armyOne.add(unit2);
+
+        armyTwo.add(unit3);
+        armyTwo.add(unit4);
+
+        Battle battle = new Battle(armyOne, armyTwo);
+        Army winner = battle.simulate();
+        assertTrue(winner instanceof Army);
+    }
 }
