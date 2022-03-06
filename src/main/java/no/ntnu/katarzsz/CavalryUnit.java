b5730b8b26e252a667860f5338462b4db47a8e00
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 16:02:38 +0100

    Added javadoc

--- src/main/java/no/ntnu/katarzsz/CavalryUnit.java
+++ src/main/java/no/ntnu/katarzsz/CavalryUnit.java
@@ -1,15 +1,33 @@
 package no.ntnu.katarzsz;
 
+/**
+ * CavalryUnit class which extends Unit
+ */
 public class CavalryUnit extends Unit {
-
+    /**
+     * CavalryUnit constructor with all params
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
     public CavalryUnit(String name, int health, int attack, int armor) {
         super(name, health, attack, armor);
     }
 
+    /**
+     * Cavalry constructor with only name and health param
+     * @param name
+     * @param health
+     */
     public CavalryUnit(String name, int health) {
         super(name, health, 20, 12);
     }
 
+    /**
+     * Gets attack bonus if it's first time it's attacking
+     * @return attack bonus if attack count is 0
+     */
     public int getAttackBonus() {
         if (this.attackCount == 0) {
             return 6;
