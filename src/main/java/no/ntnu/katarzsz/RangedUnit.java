88d96c82fa738c1714901bf4abc1068649c98429
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 15:58:43 +0100

    Added javadoc

--- src/main/java/no/ntnu/katarzsz/RangedUnit.java
+++ src/main/java/no/ntnu/katarzsz/RangedUnit.java
@@ -1,11 +1,25 @@
 package no.ntnu.katarzsz;
 
+/**
+ * RangedUnit class which extends Unit
+ */
 public class RangedUnit extends Unit{
-
+    /**
+     * RangedUnit constructor with all params
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
     public RangedUnit(String name, int health, int attack, int armor) {
         super(name, health, attack, armor);
     }
 
+    /**
+     * RangedUnit's second constructor with only name and health params
+     * @param name
+     * @param health
+     */
     public RangedUnit(String name, int health) {
         super(name, health, 15,8);
     }
@@ -14,8 +28,11 @@ public class RangedUnit extends Unit{
         return 3;
     }
 
+    /**
+     * Gets resist bonus based on how many times it was attacked
+     * @return resist bonus value
+     */
     public int getResistBonus() {
-
         if (this.gotAttackedCount == 0) {
             return 6;
         } else if (this.gotAttackedCount == 1) {
