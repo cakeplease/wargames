8bcaaf87cc8250d0d41bcc7d56aba1205fb1e570
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 16:10:13 +0100

    Added javadoc

--- src/main/java/no/ntnu/katarzsz/Battle.java
+++ src/main/java/no/ntnu/katarzsz/Battle.java
@@ -1,29 +1,73 @@
 package no.ntnu.katarzsz;
 
+/**
+ * Battle class
+ */
 public class Battle {
     private Army armyOne;
     private Army armyTwo;
 
     private boolean hasWinner = false;
 
+    /**
+     * Battle constructor
+     * @param armyOne
+     * @param armyTwo
+     */
     public Battle(Army armyOne, Army armyTwo) {
         this.armyOne = armyOne;
         this.armyTwo = armyTwo;
     }
 
-    private Army simulate() {
-        while (this.hasWinner == false) {
-            if (this.armyOne.hasUnits() && this.armyTwo.hasUnits()) {
-                this.armyOne.getRandom().attack(this.armyTwo.getRandom());
-                if (this.armyTwo.hasUnits()) {
-                    this.armyTwo.getRandom().attack(this.armyOne.getRandom());
+    /**
+     * Simulates a battle until hasWinner is true.
+     * Battle is won when one of the armies is empty of units.
+     * @return winner army
+     */
+    public Army simulate() {
+        Unit randomUnit1;
+        Unit randomUnit2;
+
+        while (!hasWinner) {
+            if (armyOne.hasUnits() && armyTwo.hasUnits()) {
+                System.out.println("------New round!------");
+                System.out.println("Both armies have units left.");
+                System.out.println("Units from ArmyOne: "+armyOne.getAllUnits().toString());
+                System.out.println("Units from ArmyTwo: "+armyTwo.getAllUnits().toString());
+
+                randomUnit1 = armyOne.getRandom();
+                randomUnit2 = armyTwo.getRandom();
+
+                System.out.println(randomUnit1 +" attacks "+ randomUnit2);
+                randomUnit1.attack(randomUnit2);
+                System.out.println("Unit\'s health after attack: "+randomUnit2.getHealth());
+
+                if (randomUnit2.getHealth() == 0) {
+                    System.out.println(randomUnit2+ " is getting removed from army.");
+                    armyTwo.remove(randomUnit2);
                 }
-            } else if (!this.armyOne.hasUnits()) {
-                this.hasWinner = true;
-                return this.armyOne;
-            } else if (!this.armyTwo.hasUnits()) {
-                this.hasWinner = true;
-                return this.armyTwo;
+                if (armyTwo.hasUnits()) {
+                    randomUnit1 = armyOne.getRandom();
+                    randomUnit2 = armyTwo.getRandom();
+
+                    System.out.println(randomUnit2 +" attacks "+ randomUnit1);
+
+                    randomUnit2.attack(randomUnit1);
+                    System.out.println("Unit\'s health after attack: "+randomUnit1.getHealth());
+
+                    if (randomUnit1.getHealth() == 0) {
+                        System.out.println(randomUnit1+ " is getting removed from army.");
+                        armyOne.remove(randomUnit1);
+                    }
+                }
+            } else if (!armyOne.hasUnits()) {
+                System.out.println("ArmyTwo won!");
+                hasWinner = true;
+                return armyTwo;
+            } else if (!armyTwo.hasUnits()) {
+                System.out.println("ArmyOne won!");
+                hasWinner = true;
+                return armyOne;
             }
         }
         return null;
