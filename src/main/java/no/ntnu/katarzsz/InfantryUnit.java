28a6f38f9fbfd1a1c4c7e8fb693004bbac78695a
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-06 15:59:42 +0100

    Added javadoc

--- src/main/java/no/ntnu/katarzsz/InfantryUnit.java
+++ src/main/java/no/ntnu/katarzsz/InfantryUnit.java
@@ -1,6 +1,16 @@
 package no.ntnu.katarzsz;
 
+/**
+ * InfantryUnit class which extends Unit
+ */
 public class InfantryUnit extends Unit {
+    /**
+     * Infantry constructor
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
     public InfantryUnit(String name, int health, int attack, int armor) {
         super(name, health, attack, armor);
     }
