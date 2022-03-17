fefc941173218faf00399b2bd8f3c0d06a5e722c
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-17 12:34:15 +0100

    Removed all prints in simulate() method

--- src/main/java/no/ntnu/katarzsz/Battle.java
+++ src/main/java/no/ntnu/katarzsz/Battle.java
@@ -4,15 +4,15 @@ package no.ntnu.katarzsz;
  * Battle class
  */
 public class Battle {
-    private Army armyOne;
-    private Army armyTwo;
+    final Army armyOne;
+    final Army armyTwo;
 
     private boolean hasWinner = false;
 
     /**
      * Battle constructor
-     * @param armyOne
-     * @param armyTwo
+     * @param armyOne army 1
+     * @param armyTwo army 2
      */
     public Battle(Army armyOne, Army armyTwo) {
         this.armyOne = armyOne;
@@ -30,33 +30,23 @@ public class Battle {
 
         while (!hasWinner) {
             if (armyOne.hasUnits() && armyTwo.hasUnits()) {
-                System.out.println("------New round!------");
-                System.out.println("Both armies have units left.");
-                System.out.println("Units from ArmyOne: "+armyOne.getAllUnits().toString());
-                System.out.println("Units from ArmyTwo: "+armyTwo.getAllUnits().toString());
 
                 randomUnit1 = armyOne.getRandom();
                 randomUnit2 = armyTwo.getRandom();
 
-                System.out.println(randomUnit1 +" attacks "+ randomUnit2);
                 randomUnit1.attack(randomUnit2);
-                System.out.println("Unit\'s health after attack: "+randomUnit2.getHealth());
 
                 if (randomUnit2.getHealth() == 0) {
-                    System.out.println(randomUnit2+ " is getting removed from army.");
                     armyTwo.remove(randomUnit2);
                 }
+
                 if (armyTwo.hasUnits()) {
                     randomUnit1 = armyOne.getRandom();
                     randomUnit2 = armyTwo.getRandom();
 
-                    System.out.println(randomUnit2 +" attacks "+ randomUnit1);
-
                     randomUnit2.attack(randomUnit1);
-                    System.out.println("Unit\'s health after attack: "+randomUnit1.getHealth());
 
                     if (randomUnit1.getHealth() == 0) {
-                        System.out.println(randomUnit1+ " is getting removed from army.");
                         armyOne.remove(randomUnit1);
                     }
                 }
